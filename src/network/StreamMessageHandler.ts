3c3797ddd98d40eafa2d81dac47e6921d9cde162
Ilya <cankillah@gmail.com>
2026-02-18 04:17:45 +0200

    rollback url

--- src/network/StreamMessageHandler.ts
+++ src/network/StreamMessageHandler.ts
@@ -40,7 +40,7 @@ export class StreamMessageHandler {
     private eventCounter: number = 0;
     private lastMessageTime: Date | null = null;
     
-    private constructor(url: string = 'wss://python-stream-handler.onrender.com/ws') {
+    private constructor(url: string = 'wss://python-stream-handler.onrender.com/') {
         this.url = url;
         this.initListeners();
     }
