6bdf3a58b535677d35ce57affdf248d7e2685761
Ilya <cankillah@gmail.com>
2026-02-26 17:11:32 +0200

    [moving to vps + port change]

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
 
   devServer: {
     historyApiFallback: true,
-    port: 80,
+    port: 8008,
     /*https: {
       key: fs.readFileSync('./cert/key.pem'),
       cert: fs.readFileSync('./cert/cert.pem'),
